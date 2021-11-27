66ffb8e01c5dc4acb2a912740f007613e50b6543
savitakumbhare <savita.kumbhare21@gmail.com>
2021-11-27 06:00:44 +0530

    Updated Database Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -55,5 +55,4 @@ ActiveRecord::Schema.define(version: 2021_11_26_131853) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
